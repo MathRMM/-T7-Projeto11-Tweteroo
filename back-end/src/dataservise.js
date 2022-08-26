0aeb6465da065d71402a0d857720e077af4a5d85
Math <math@pop-os.localdomain>
2022-08-26 15:04:04 -0300

    finalizado

---
+++ back-end/src/dataservise.js
@@ -0,0 +1,20 @@
+const user = [{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+},{
+	username: 'zoroSolador', 
+	avatar: "https://preview.redd.it/jeu9b91a7g381.jpg?auto=webp&s=9cc031a6f548ea0f8b9feec1b8aff854a1d8e25e" 
+},{
+	username: 'kaizokuoni', 
+	avatar: "https://nntheblog.b-cdn.net/wp-content/uploads/2022/04/luffy.jpg" 
+}]
+
+const tweets = [{
+	username: "bobesponja",
+  tweet: "eu amo o hub"
+},{
+    username: 'kaizokuoni',
+    tweet:'Quem é esse tal de joyboy?'
+}]
+
+export {user,tweets}
---
+++ back-end/src/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import cors from "cors";
+import { user, tweets } from "./dataservise.js";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+// manipulação de dados
+let allTweets = [];
+
+function GET_TWEETS(numberTweets) {
+    allTweets = []
+    if (tweets.length > numberTweets) numberTweets = tweets.length - numberTweets;
+    else numberTweets = 0
+
+    for(let j= user.length-1; j>=0;j--) {
+        for (let i = tweets.length-1; i >= numberTweets; i--) {
+
+            if (user[j].username === tweets[i].username) {
+                let tweet = tweets[i].tweet;
+                allTweets.push({
+                    ...user[j],
+                    tweet,
+                });
+            }
+        }
+    };
+    console.log('alltweets '+allTweets.length)
+}
+
+// servidor GETS
+server.get("/tweets", function (req, res) {
+    let query = Number(req.query.page)
+    if(!query||query<1){ res.status(400).send('Informe uma página válida!'); return}
+    let numberTweets = 10 * query;
+    GET_TWEETS(numberTweets);
+    console.log(query)
+    res.send(allTweets);
+
+});
+server.get("/tweets/:USERNAME", function (req, res) {
+    let user = req.params.USERNAME;
+    let userTweets = allTweets.find((value) => value.username === user);
+    res.send(userTweets);
+});
+
+
+// servidor POSTS
+server.post("/sign-up", function (req, res) {
+    let userSingUp = req.body;
+    console.log(userSingUp)
+    if(userSingUp.username===''||userSingUp.avatar===''){
+        res.sendStatus(400)
+        return
+    }
+    user.push(userSingUp);
+    res.status(201).send("OK");
+});
+server.post("/tweets", function (req, res) {
+    let newTweets = req.body;
+    let _username = req.headers.user;
+    let authenticate = user.find(value => value.username === _username)
+    if(!authenticate || newTweets.tweet===''){
+        res.sendStatus(400)
+        return
+    }
+    tweets.push({
+        username: _username,
+        ...newTweets,
+    });
+    res.status(201).send("OK");
+});
+
+server.listen(5000);
